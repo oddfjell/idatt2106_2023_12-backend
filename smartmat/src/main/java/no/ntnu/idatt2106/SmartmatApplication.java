b3a33c7bb73286f0f1fae49a6fb08dd09dd6a248
SchatzerZD <daniel.walaker@gmail.com>
2023-04-18 09:41:31 +0200

    Initialization of project

---
+++ smartmat/src/main/java/no/ntnu/idatt2106/SmartmatApplication.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2106;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartmatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartmatApplication.class, args);
+    }
+
+}
---
+++ smartmat/src/test/java/no/ntnu/idatt2106/SmartmatApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idatt2106;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartmatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
