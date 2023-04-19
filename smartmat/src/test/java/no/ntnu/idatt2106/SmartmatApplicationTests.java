34d63bb918212b2f4cdc82612b2278c7f3ef3e81
EmilOrv <emilols.eo@gmail.com>
2023-04-19 10:27:11 +0200

    Test of testing

--- smartmat/src/test/java/no/ntnu/idatt2106/SmartmatApplicationTests.java
+++ smartmat/src/test/java/no/ntnu/idatt2106/SmartmatApplicationTests.java
@@ -3,11 +3,14 @@ package no.ntnu.idatt2106;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @SpringBootTest
 class SmartmatApplicationTests {
 
     @Test
     void contextLoads() {
+        assertFalse(true);
     }
 
 }
