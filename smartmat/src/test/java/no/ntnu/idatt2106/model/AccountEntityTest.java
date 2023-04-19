8ba693938000458d3ef2af87f5cbab9b328c727d
SchatzerZD <daniel.walaker@gmail.com>
2023-04-19 10:29:41 +0200

    Added tests for getters and setter in AccountEntityTest

--- smartmat/src/test/java/no/ntnu/idatt2106/model/AccountEntityTest.java
+++ smartmat/src/test/java/no/ntnu/idatt2106/model/AccountEntityTest.java
@@ -7,23 +7,45 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AccountEntityTest {
 
+    AccountEntity account;
+
     @BeforeEach
     void setUp() {
+
+        account = new AccountEntity();
+        account.setUsername("daniel");
+        account.setPassword("yoyo123");
+
+
     }
 
     @Test
     void getUsername() {
+        assertEquals("daniel",account.getUsername());
+        assertNotEquals("abhi",account.getUsername());
+        assertNotNull(account.getUsername());
     }
 
     @Test
     void setUsername() {
+        account.setUsername("ingrid");
+        assertEquals("ingrid",account.getUsername());
+        assertNotEquals("daniel",account.getUsername());
+        assertNotNull(account.getUsername());
     }
 
     @Test
     void getPassword() {
+        assertEquals("yoyo123",account.getPassword());
+        assertNotEquals("hei123",account.getPassword());
+        assertNotNull(account.getPassword());
     }
 
     @Test
     void setPassword() {
+        account.setPassword("hei123");
+        assertEquals("hei123",account.getPassword());
+        assertNotEquals("yoyo123",account.getPassword());
+        assertNotNull(account.getPassword());
     }
 }
