6a27bc1e7a638eb806c037558162fbe89d927784
EmilOrv <emilols.eo@gmail.com>
2023-04-19 10:29:12 +0200

    Updated tests

--- smartmat/src/test/java/no/ntnu/idatt2106/SmartmatApplicationTests.java
+++ smartmat/src/test/java/no/ntnu/idatt2106/SmartmatApplicationTests.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SmartmatApplicationTests {
 
     @Test
     void contextLoads() {
-        assertFalse(true);
+        assertTrue(true);
     }
 
 }
