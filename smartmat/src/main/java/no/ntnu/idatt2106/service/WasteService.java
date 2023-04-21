7187bc11ad4103d2426b8abb4248d4e4ab3aa2ea
Jonas Mørkland <jonasmorkland@outlook.com>
2023-04-21 13:06:52 +0200

    Created endpoint for retrieving total waste per user in NOK. It finds all rows corresponding to a account and sums up the column 'money_lost'.

--- smartmat/src/main/java/no/ntnu/idatt2106/service/WasteService.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/service/WasteService.java
@@ -1,7 +1,16 @@
 package no.ntnu.idatt2106.service;
 
+import no.ntnu.idatt2106.repository.WasteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class WasteService {
+
+    @Autowired
+    private WasteRepository wasteRepository;
+
+    public int getMoneyLost(long id) {
+        return wasteRepository.getMoneyLost(id);
+    }
 }
