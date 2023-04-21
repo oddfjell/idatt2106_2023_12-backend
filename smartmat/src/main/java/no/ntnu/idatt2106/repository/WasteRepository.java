ea5a05d217a1a25b102b920accecb05156873416
Jonas Mørkland <jonasmorkland@outlook.com>
2023-04-21 14:59:05 +0200

    Edited endpoints for retrieving lost money be returning 0 if no rows were found.

--- smartmat/src/main/java/no/ntnu/idatt2106/repository/WasteRepository.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/repository/WasteRepository.java
@@ -3,12 +3,13 @@ package no.ntnu.idatt2106.repository;
 import no.ntnu.idatt2106.model.WasteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import java.util.Optional;
 
 public interface WasteRepository extends JpaRepository<WasteEntity, Long> {
 
     @Query("SELECT SUM(e.money_lost) FROM WasteEntity e WHERE e.accountEntity.id = :id")
-    int getMoneyLost(long id);
+    Optional<Integer> getMoneyLost(long id);
 
     @Query("SELECT SUM(e.money_lost) FROM WasteEntity e WHERE e.accountEntity.id = :id AND e.month = :monthNumber")
-    int getMoneyLostPerMonth(long id, int monthNumber);
+    Optional<Integer> getMoneyLostPerMonth(long id, int monthNumber);
 }
