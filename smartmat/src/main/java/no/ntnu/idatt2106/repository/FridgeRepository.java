7f61ed568a82d2c77e5501772d284652ee30250c
SchatzerZD <daniel.walaker@gmail.com>
2023-04-21 13:04:01 +0200

    Added fridge service method functionality

--- smartmat/src/main/java/no/ntnu/idatt2106/repository/FridgeRepository.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/repository/FridgeRepository.java
@@ -4,6 +4,8 @@ import no.ntnu.idatt2106.model.AccountEntity;
 import no.ntnu.idatt2106.model.FridgeEntity;
 import no.ntnu.idatt2106.model.GroceryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +13,11 @@ import java.util.Optional;
 public interface FridgeRepository extends JpaRepository<FridgeEntity, Long> {
 
     List<FridgeEntity> findAllByAccountEntity(AccountEntity account);
-    FridgeEntity findByAccountEntityAndGroceryEntity(AccountEntity account, GroceryEntity grocery);
+    Optional<FridgeEntity> findByAccountEntityAndGroceryEntity(AccountEntity account, GroceryEntity grocery);
+
+    @Modifying
+    @Query("update FridgeEntity f set f.count=?1 where f.accountEntity=?2 and f.groceryEntity=?3")
+    void updateCount(int count, AccountEntity account, GroceryEntity grocery);
+
 
 }
--- smartmat/src/main/java/no/ntnu/idatt2106/service/FridgeService.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/service/FridgeService.java
@@ -2,7 +2,8 @@ package no.ntnu.idatt2106.service;
 
 
 import jakarta.transaction.Transactional;
-import no.ntnu.idatt2106.exceptions.UserDoesntExistException;
+import no.ntnu.idatt2106.exceptions.AccountAlreadyHasGroceryException;
+import no.ntnu.idatt2106.exceptions.AccountDoesntExistException;
 import no.ntnu.idatt2106.model.AccountEntity;
 import no.ntnu.idatt2106.model.FridgeEntity;
 import no.ntnu.idatt2106.model.GroceryEntity;
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -40,10 +42,14 @@ public class FridgeService {
 
     }
 
-    public void addGroceryToAccount(AccountEntity account, GroceryEntity grocery, int count) throws UserDoesntExistException {
+    public void addGroceryToAccount(AccountEntity account, GroceryEntity grocery, int count) throws AccountDoesntExistException, AccountAlreadyHasGroceryException {
 
         if(accountRepository.findByUsernameIgnoreCase(account.getUsername()).isEmpty()){
-            throw new UserDoesntExistException();
+            throw new AccountDoesntExistException();
+        }
+        Optional<FridgeEntity> optionalFridgeEntity = fridgeRepository.findByAccountEntityAndGroceryEntity(account,grocery);
+        if(optionalFridgeEntity.isPresent()){
+            throw new AccountAlreadyHasGroceryException();
         }
 
         FridgeEntity fridgeEntity = new FridgeEntity();
@@ -54,12 +60,24 @@ public class FridgeService {
         fridgeRepository.save(fridgeEntity);
     }
 
-    public void removeGroceryFromAccount(AccountEntity account, GroceryEntity grocery){
+    public void updateGroceryCount(AccountEntity account, GroceryEntity grocery, int count){
+        fridgeRepository.updateCount(count,account,grocery);
+    }
 
+    public void removeGroceryFromAccount(AccountEntity account, GroceryEntity grocery){
+        Optional<FridgeEntity> optionalFridgeEntity = fridgeRepository.findByAccountEntityAndGroceryEntity(account,grocery);
+        optionalFridgeEntity.ifPresent(fridgeEntity -> fridgeRepository.delete(fridgeEntity));
     }
 
     public void removeGroceryFromAccountByAmount(AccountEntity account, GroceryEntity grocery, int count){
+        Optional<FridgeEntity> optionalFridgeEntity = fridgeRepository.findByAccountEntityAndGroceryEntity(account,grocery);
+        optionalFridgeEntity.ifPresent(fridgeEntity -> {
+            if(count > fridgeEntity.getCount()){
+                throw new IllegalArgumentException();
+            }
 
+            fridgeRepository.updateCount(fridgeEntity.getCount() - count, account, grocery);
+        });
     }
 
     public void throwGroceryFromAccount(AccountEntity account, GroceryEntity grocery){
