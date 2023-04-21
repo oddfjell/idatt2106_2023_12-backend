318d0c342d3a0c38a90877be6287df92bef35e04
Ann-Marie Revillard <ann.marie.amr@gmail.com>
2023-04-21 09:56:40 +0200

    Commented out wrong/not done things

--- smartmat/src/main/java/no/ntnu/idatt2106/service/ShoppingListService.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/service/ShoppingListService.java
@@ -12,9 +12,11 @@ public class ShoppingListService {
     private ShoppingListRepository shoppingListRepository;
 
     public boolean addToShoppingList(ShoppingListEntity product){
+        return false;
+
         //TODO save må legge til et produkt og/eller øke amount
         // kan være forskjellige metoder
-        int before;
+        /**int before;
         if(shoppingListRepository.doContain(product)){
             before = shoppingListRepository.getAmount(product.getFullName());
         } else before = 0;
@@ -22,15 +24,17 @@ public class ShoppingListService {
         shoppingListRepository.save(product);
         int after = shoppingListRepository.getAmount(product.getFullName());
 
-        return before == (after - 1);
+        return before == (after - 1);*/
     }
 
     public boolean removeFromShoppingList(ShoppingListEntity product){
+        return false;
+
         //TODO DO NOT DELETE IF THE PRODUCT AMOUNT IS MORE THAN 1
-        int before = shoppingListRepository.findAll().size();
+        /**int before = shoppingListRepository.findAll().size();
         shoppingListRepository.delete(product);
         int after = shoppingListRepository.findAll().size();
-        return before == (after + 1);
+        return before == (after + 1);*/
     }
 
     public void acceptRequest(ShoppingListEntity product){//TODO ta imot en slags form for id
