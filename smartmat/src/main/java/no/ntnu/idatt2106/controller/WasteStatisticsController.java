a229c81046e3a343fd60f1d2834c0fa88cce6d47
Jonas Mørkland <jonasmorkland@outlook.com>
2023-04-21 14:04:30 +0200

    Edited endpoint for getting total money lost by using '@AuthenticationPrincipal'.

--- smartmat/src/main/java/no/ntnu/idatt2106/controller/WasteStatisticsController.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/controller/WasteStatisticsController.java
@@ -1,9 +1,11 @@
 package no.ntnu.idatt2106.controller;
 
+import no.ntnu.idatt2106.model.AccountEntity;
 import no.ntnu.idatt2106.service.WasteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "/waste") //TODO AUTH??
@@ -13,8 +15,8 @@ public class WasteStatisticsController {
     @Autowired
     private WasteService wasteService;
 
-    @GetMapping("/{id}")
-    public ResponseEntity<?> geMoneyLost(@PathVariable long id){
-        return new ResponseEntity<>(wasteService.getMoneyLost(id), HttpStatus.OK);
+    @GetMapping("/")
+    public ResponseEntity<?> geMoneyLost(@AuthenticationPrincipal AccountEntity account){
+        return new ResponseEntity<>(wasteService.getMoneyLost(account.getAccount_id()), HttpStatus.OK);
     }
 }
