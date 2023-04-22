1690700bd23ab6ef9b7bb832a3806334d4957002
EmilOrv <emiloo@stud.ntnu.no>
2023-04-22 12:52:53 +0200

    Updated CrossOrigin

--- smartmat/src/main/java/no/ntnu/idatt2106/controller/FridgeController.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/controller/FridgeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:5173/", allowCredentials = "true")
 @RequestMapping(value = "/fridge")
 public class FridgeController {
 
--- smartmat/src/main/java/no/ntnu/idatt2106/controller/GroceryController.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/controller/GroceryController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RequestMapping(value = "/grocery")
 @RestController
+@CrossOrigin(origins = "http://localhost:5173/", allowCredentials = "true")
 public class GroceryController {
 
     @Autowired
