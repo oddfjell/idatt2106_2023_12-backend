b13daf208d6790788649e6e932c0517e39459ab9
Jonas Mørkland <jonasmorkland@outlook.com>
2023-04-24 09:30:14 +0200

    Added endpoint for getting waste for a user per category.

--- smartmat/src/main/java/no/ntnu/idatt2106/service/WasteService.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/service/WasteService.java
@@ -4,6 +4,9 @@ import no.ntnu.idatt2106.repository.WasteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.List;
+
 @Service
 public class WasteService {
 
@@ -14,11 +17,17 @@ public class WasteService {
         return wasteRepository.getMoneyLost(id).orElse(0);
     }
 
+    public int getMoneyLostByCategory(long id, long categoryId) {
+        return wasteRepository.getMoneyLostByCategory(id, categoryId).orElse(0);
+    }
+
+    public List<List> getMoneyLostPerCategory(long id) {
+        return wasteRepository.getMoneyLostPerCategory(id);
+    }
+
     public int getMoneyLostPerMonth(long id, int monthNumber) {
         return wasteRepository.getMoneyLostPerMonth(id, monthNumber).orElse(0);
     }
 
-    public int getMoneyLostByCategory(long id, long categoryId) {
-        return wasteRepository.getMoneyLostByCategory(id, categoryId).orElse(0);
-    }
+
 }
