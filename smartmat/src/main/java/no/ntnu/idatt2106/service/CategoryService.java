1d5210941369ba51b73206562f3394216f67deb8
Ann-Marie Revillard <ann.marie.amr@gmail.com>
2023-04-28 17:46:09 +0200

    Added some loggers

--- smartmat/src/main/java/no/ntnu/idatt2106/service/CategoryService.java
+++ smartmat/src/main/java/no/ntnu/idatt2106/service/CategoryService.java
@@ -3,6 +3,8 @@ package no.ntnu.idatt2106.service;
 import jakarta.transaction.Transactional;
 import no.ntnu.idatt2106.model.CategoryEntity;
 import no.ntnu.idatt2106.repository.CategoryRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,12 +15,15 @@ public class CategoryService {
   @Autowired
   CategoryRepository categoryRepository;
 
+  private static final Logger logger = LoggerFactory.getLogger(CategoryService.class);
+
   public void addCategory(CategoryEntity category){
+    logger.info("Saving category {}", category.getName());
     categoryRepository.save(category);
   }
 
   public void removeCategory(CategoryEntity category){
+    logger.info("Removing category {}", category.getName());
     categoryRepository.removeById(category.getCategory_id());
   }
-
 }
